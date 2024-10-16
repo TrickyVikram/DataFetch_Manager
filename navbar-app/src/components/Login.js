8a21936788d2740f6325e89f06ec860770fabb46
Vikram kumar <114013697+TrickyVikram@users.noreply.github.com>
2024-10-17 01:17:08 +0530

    Update Login.js

--- navbar-app/src/components/Login.js
+++ navbar-app/src/components/Login.js
@@ -9,19 +9,19 @@ const Login = () => {
   return (
     <>
       {/* Bootstrap Navbar */}
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a className="navbar-brand" href="#">MyApp</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto">
-            <li className="nav-item">
-              <button className="btn btn-primary" onClick={handleShow}>Login</button>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      // <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      //   <a className="navbar-brand" href="#">MyApp</a>
+      //   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      //     <span className="navbar-toggler-icon"></span>
+      //   </button>
+      //   <div className="collapse navbar-collapse" id="navbarNav">
+      //     <ul className="navbar-nav ml-auto">
+      //       <li className="nav-item">
+      //         <button className="btn btn-primary" onClick={handleShow}>Login</button>
+      //       </li>
+      //     </ul>
+      //   </div>
+      // </nav>
 
       {/* Login Modal */}
       {show && (
